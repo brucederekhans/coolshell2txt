9bbf1432b46d61360b1a869a0a0f301cd723ff81
brucederekhans <brucederekhans@gmail.com>
2023-05-17 11:14:08 +0800

    make anchor element visible in page

--- runInConsole.js
+++ runInConsole.js
@@ -4,4 +4,6 @@ let blob = new Blob([textContent], {type:"text/plain"});
 let anchorElement = document.createElement("a");
 anchorElement.href = URL.createObjectURL(blob);
 anchorElement.download = title;
+anchorElement.textContent = "save as";
+document.querySelector(".post-content").insertBefore(anchorElement, document.querySelector(".entry-content"));
 anchorElement.click();
