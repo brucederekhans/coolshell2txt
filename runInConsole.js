d5aa9df40ed58c93ac5e97a0224fd201eaeb03e1
brucederekhans <brucederekhans@gmail.com>
2023-05-18 09:45:19 +0800

    delete content of last 3 children in div.entry-content

--- runInConsole.js
+++ runInConsole.js
@@ -1,4 +1,10 @@
-let textContent = document.querySelector(".entry-content").textContent;
+let lastChildDiv1TextContent = document.querySelector(".entry-content > div:nth-last-child(1)").textContent;
+let lastChildDiv2TextContent = document.querySelector(".entry-content > div:nth-last-child(2)").textContent;
+let lastChildDiv3TextContent = document.querySelector(".entry-content > div:nth-last-child(3)").textContent;
+let textContent = document.querySelector(".entry-content").textContent
+    .replace(lastChildDiv1TextContent, "")
+    .replace(lastChildDiv2TextContent, "")
+    .replace(lastChildDiv3TextContent, "");
 let title = document.querySelector(".entry-title").textContent;
 let blob = new Blob([textContent], {type:"text/plain"});
 let anchorElement = document.createElement("a");
