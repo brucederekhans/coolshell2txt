b53d5cb9bb74c6f2b59685f6a82caa28c4224b8d
brucederekhans <brucederekhans@gmail.com>
2023-05-18 09:49:27 +0800

    delete content of last 3 children in div.entry-content, close #1

--- coolshell2txt.user.js
+++ coolshell2txt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CoolShell2txt
 // @namespace    https://github.com/brucederekhans/coolshell2txt
-// @version      0.1
+// @version      0.2
 // @description  save an article in coolshell.cn as text file
 // @author       brucederekhans
 // @match        *://coolshell.cn/articles/*
@@ -11,7 +11,13 @@
 (function() {
     'use strict';
 
-    let textContent = document.querySelector(".entry-content").textContent;
+    let lastChildDiv1TextContent = document.querySelector(".entry-content > div:nth-last-child(1)").textContent;
+    let lastChildDiv2TextContent = document.querySelector(".entry-content > div:nth-last-child(2)").textContent;
+    let lastChildDiv3TextContent = document.querySelector(".entry-content > div:nth-last-child(3)").textContent;
+    let textContent = document.querySelector(".entry-content").textContent
+        .replace(lastChildDiv1TextContent, "")
+        .replace(lastChildDiv2TextContent, "")
+        .replace(lastChildDiv3TextContent, "");
     let title = document.querySelector(".entry-title").textContent;
     let blob = new Blob([textContent], {type:"text/plain"});
     let anchorElement = document.createElement("a");
